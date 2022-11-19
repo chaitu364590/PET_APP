579d23a7ac89d21ae400a470f927e0fc54f745d1
chaitu364590 <84063946+chaitu364590@users.noreply.github.com>
2022-11-19 12:49:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 import PIL
 from CHECK_SSIM import show_growth_page
 from predict_page import show_predict_page
-from diff import show_diif_page
+from diff import show_diff_page
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
